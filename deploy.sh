8f5c6eee11525d8fd54b196b18d66c11248ee594
horup <soh@skov.dk>
2025-08-05 13:07:49 +0200

    added title and favicon

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/obby.wasm dist/obby.wasm
 cp index.html dist/index.html
 cp mq_js_bundle.js dist/mq_js_bundle.js
+cp favicon.png dist/favicon.png
 cp -r res dist/res
